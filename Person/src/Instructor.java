713055751e3886408dc07206841fe24906928840
kristina <kristina@localhost>
2022-04-05 12:28:57 -0400

    version 1

---
+++ Person/src/Instructor.java
@@ -0,0 +1,18 @@
+
+public class Instructor extends Person 
+{
+	private double salary;
+
+	
+		public Instructor(String name, int birthYear, double salary)
+		{
+			super(name, birthYear);
+			this.salary = salary;
+		}
+
+		public String toString()
+		{
+			return "Instructor[super = " + super.toString() + ", salary = " + salary + "]";
+		}
+
+}
---
+++ Person/src/Student.java
@@ -0,0 +1,21 @@
+
+public class Student extends Person
+{
+	private String major;
+
+
+	  public Student(String name, int birthYear, String major)
+	  {
+	     super(name, birthYear);
+	     this.major = major;
+	     
+	  }
+
+
+	   public String toString()
+	   {
+	      return "Student[super = " + super.toString() + ", major = " + major + "]";
+	   }
+
+
+}
